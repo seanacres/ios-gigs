644b10a4bcfe1bb1c059c51fb041a45d16a9cfbf
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-19 16:16:32 -0400

    Set up user and bearer structs

---
+++ Gigs/Gigs/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Sean Acres on 6/19/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer {
+    let token: String
+}
